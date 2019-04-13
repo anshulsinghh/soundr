8eada52c41767adefe6df3923f7e22e27c39c677
alecvaldez <a1valdez@ucsd.edu>
2019-04-13 01:18:05 -0700

    TEs

--- Soundr/src/gui.java
+++ Soundr/src/gui.java
@@ -1,5 +1,11 @@
+import javafx.*;
 public class gui {
     public static void main(String[] args) {
+        int[] oof = {1,2,0,5,4};
+        Arrays.sort(oof);
+        for(int i = 0; i < oof.length; i++) {
+            System.out.println(oof[i]);
+        }
         System.out.println("Hello");
     }
 }
