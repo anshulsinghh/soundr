2714207012c5463d8ef29b33e074d2763b80c512
alecvaldez <a1valdez@ucsd.edu>
2019-04-13 06:58:36 -0700

    Gui Layout

--- Soundr/src/gui.java
+++ Soundr/src/gui.java
@@ -1,8 +1,10 @@
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
@@ -11,89 +13,108 @@ import javafx.scene.control.ToolBar;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.event.ActionEvent;
-
-
 
 public class gui extends Application {
 
-  private static final int WINDOW_WIDTH = 1190;
-  private static final int WINDOW_HEIGHT = 830;
-  private static final int TITLEBAR_HEIGHT = 65;
-  
-//  private double offsetX = 0;
-  //private double offsetY = 0;
-  
   class TitleBarButtons extends HBox {
-    
+
     public TitleBarButtons() {
-      Button minButton = new Button("-");
-      Button maxButton = new Button("F");
-      Button closeButton = new Button("X");
-      
+
+      Button minButton = new Button("\uE949");
+      Button maxButton = new Button("\uE739");
+      Button closeButton = new Button("\uE106");
+
+      minButton.setId("min");
+      maxButton.setId("max");
+      closeButton.setId("close");
+
       this.getChildren().add(minButton);
       this.getChildren().add(maxButton);
       this.getChildren().add(closeButton);
+      this.setPadding(new Insets(0, 0, 13, 0));
       minButton.setOnAction(new EventHandler<ActionEvent>() {
-        
+
         @Override
         public void handle(ActionEvent a) {
-          Platform.exit();
+          setIconified(true);
         }
-        
+
       });
-      
+
       maxButton.setOnAction(new EventHandler<ActionEvent>() {
-        
+
         @Override
         public void handle(ActionEvent a) {
           Platform.exit();
         }
-        
+
       });
-      
+
       closeButton.setOnAction(new EventHandler<ActionEvent>() {
-        
+
         @Override
         public void handle(ActionEvent a) {
           Platform.exit();
         }
-        
+
       });
-      
+
     }
   }
   
+  double offsetX = 0;
+  double offsetY = 0;
+  
+  private Stage stage;
+
   public static void main(String[] args) {
     launch(args);
   }
 
   @Override
   public void start(Stage stage) throws Exception {
-    Image background = new Image("/image/background.jpg");
-    ImageView imView = new ImageView(background);
     
+    this.stage = stage;
     StackPane root = new StackPane();
-    root.getChildren().addAll(imView);
-    
-    Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
-    stage.initStyle(StageStyle.TRANSPARENT);
-    
+    Scene scene = new Scene(root);
+    scene.getStylesheets().add("/css/style.css");
+    this.stage.initStyle(StageStyle.TRANSPARENT);
+
     BorderPane pane = new BorderPane();
-    
+
     ToolBar titleBar = new ToolBar();
-    
-    titleBar.setPrefHeight(TITLEBAR_HEIGHT);
-    titleBar.setMinHeight(TITLEBAR_HEIGHT);
-    titleBar.setMaxHeight(TITLEBAR_HEIGHT);
-    titleBar.setStyle("-fx-background-color: transparent;");
     titleBar.getItems().add(new TitleBarButtons());
+
+    titleBar.setOnMousePressed(new EventHandler<MouseEvent>() {
+
+      @Override
+      public void handle(MouseEvent e) {
+        offsetX = stage.getX() - e.getScreenX();
+        offsetY = stage.getY() - e.getScreenY();
+      }
+
+    });
     
+    titleBar.setOnMouseDragged(new EventHandler<MouseEvent>() {
+
+      @Override
+      public void handle(MouseEvent e) {
+        stage.setX(e.getScreenX() + offsetX);
+        stage.setY(e.getScreenY() + offsetY);
+      }
+
+    });
+
     pane.setTop(titleBar);
     root.getChildren().add(pane);
-    stage.setScene(scene);    
-    stage.show();
+    this.stage.setScene(scene);
+    this.stage.show();
   }
 
+  public void setIconified(boolean bool) {
+    this.stage.setIconified(bool);
+  }
 
 }
